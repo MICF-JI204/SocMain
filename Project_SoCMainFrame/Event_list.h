2ec664908173ad44e0edbd6d1c4cbfd05d5ebcf7
tripack <tripack@126.com>
2015-03-26 19:47:52 +0800

    Incorparted interface with event list

--- Project_SoCMainFrame/Event_list.h
+++ Project_SoCMainFrame/Event_list.h
@@ -7,7 +7,7 @@ class Event_list
 {
 	public:
 		int Add_event(); // haven't complete;
-		int Exacte_event(); // zhi xing yi ge ren wu
+		int Exacute_event(); // zhi xing yi ge ren wu
 		int Regist_event();// haven't complete;
 		
 	private:
