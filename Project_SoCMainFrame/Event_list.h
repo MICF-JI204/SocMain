8b928e25ba814b5baef899df6fa106df9ef12b48
Kinghsy <136448624@qq.com>
2015-03-26 18:50:21 +0800

    。。
    。。

---
+++ Project_SoCMainFrame/Event_list.h
@@ -0,0 +1,18 @@
+#ifndef __Event_list_h__
+#define __Event_list_h__
+
+class Event_list
+{
+	public:
+		int Add_event
+		
+		
+		
+		
+	private:
+		unsigned char t_count;
+		unsigned char t_pointer;
+	
+};
+
+#endif
