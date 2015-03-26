63dd81c7b6795f8d252216e53e21baac0ae330a6
tripack <tripack@126.com>
2015-03-26 11:59:29 +0800

    Some Comments, Descriptions

---
+++ Project_SoCMainFrame/Ardos.h
@@ -0,0 +1,6 @@
+#ifndef __Ardos_h__
+#define __Ardos_h__
+
+
+
+#endif
--- Project_SoCMainFrame/os_main.c
+++ Project_SoCMainFrame/os_main.c
@@ -1,6 +1,21 @@
 #include "Arduino.h"
+#include "Ardos.h"
+
+/*This is the Main File of Ardos.
+  Ardos is an NOT-SO-GENERAL purpose operating system 
+  for Arduino Boards, samll and light weight enough to 
+  be put into arduino uno.
+  This operating system features a simple event driven
+  model, and a simple timer for NOT-REAL-TIME timer.
+  (Multithresding)
+*/
 
 int os_init(){
   
   return 0;
+}
+
+int os_run(){
+	//the main cycle of the system.
+	
 }
