bd6ddf9ed48c612d3f60457165b3ffb24418c184
Kinghsy <136448624@qq.com>
2015-03-26 19:13:43 +0800

    。。

---
+++ Project_SoCMainFrame/Event_list.cpp
@@ -0,0 +1,16 @@
+#include<Event_list.h>
+
+Event_list::Add_event() //Add 1 event
+{
+	
+}
+
+Event_list::Add_event() //Add a couple of events
+{
+	
+}
+
+Event_list::Exacte_event()
+{
+	
+}
--- Project_SoCMainFrame/Event_list.h
+++ Project_SoCMainFrame/Event_list.h
@@ -1,17 +1,20 @@
 #ifndef __Event_list_h__
 #define __Event_list_h__
 
+struct 
+
 class Event_list
 {
 	public:
-		int Add_event
-		
-		
-		
+		int Add_event(); // haven't complete;
+		int Exacte_event(); // zhi xing yi ge ren wu
+		int Regist_event();// haven't complete;
 		
 	private:
-		unsigned char t_count;
-		unsigned char t_pointer;
+		//unsigned char t_count;
+		//unsigned char t_pointer;
+		int Level[20];
+		int 
 	
 };
 
