db87854356843797485cefde9476301d15309fea
tripack <tripack@126.com>
2015-03-28 21:10:59 +0800

    Expanding OS_Status More

--- Project_SoCMainFrame/os_main.cpp
+++ Project_SoCMainFrame/os_main.cpp
@@ -15,19 +15,20 @@
 
 int os_init(){//Initialization process of the system
   
+  for(int i=0;i<lib_init_count)
   return 0;
 }
 
 int os_run(){//the main cycle of the system
-	os_status.systime=millis();//Acquire System Time
+	systime=millis();//Acquire System Time
 	
 	int* tevent=NULL; 
-	for(int i=0;i<os_status.task_list_count;i++)
+	for(int i=0;i<task_list_count;i++)
 	{//Go Through Task List
 
 		do
 		{
-			tevent=(*(os_status.tlist[i])).getTask(os_status.systime);
+			tevent=(*(tlist[i])).getTask(systime);
 			if(tevent==ERRPTR)//No more Tasks In a task list
 			{
 			   os_remove_task_list(i);//Dispose Number i Task
@@ -44,38 +45,40 @@ int os_run(){//the main cycle of the system
 }
 
 int os_add_task_list(Task_List* ntask_list){//ntask_list as in New Task List
-    if(os_status.task_list_count==MAX_TASK_LIST) return ERR_LIST_TASK_LIST_OVERFLOW; 
+    if(task_list_count==MAX_TASK_LIST) return ERR_LIST_TASK_LIST_OVERFLOW; 
 	//if no more space in the list of task_list
-	os_status.tlist[os_status.task_list_count]=ntask_list;	
+	tlist[task_list_count]=ntask_list;	
 	
 	
 	//Initialize the tasklist for further usee
-	(ntask_list->task[0]).time_set=os_status.systime+(ntask_list->task[i]).time_step
+	(ntask_list->task[0]).time_set=systime+(ntask_list->task[i]).time_step
 	//First term = Last term + time step
 	for(int i=1;i < (ntask_list->t_count);i++){
 		(ntask_list->task[i]).time_set=(ntask_list->task[i-1]).time_set+(ntask_list->task[i]).time_step;
 	}//This term = Last term + time step
 	
 	
-	os_status.task_list_count++;
+	task_list_count++;
 	return 0;
 }
 
 int os_remove_task_list(byte listnum,byte err_num){
-	if(os_status.task_list[listnum]==NULL) return ERR_REMOVE_EMPTY_LIST;
-	int (*tfunpt)(byte)=(*os_status.task_list[listnum]).disposeFunc;
+	if(task_list[listnum]==NULL) return ERR_REMOVE_EMPTY_LIST;
+	int (*tfunpt)(byte)=(*task_list[listnum]).disposeFunc;
 	if(tfuncpt!=NULL)(*tfunpt)(err_num);
 	//Try Disposing the functions,Make Sure Not NULL!!
-	os_status.task_list[listnum]=os_status.task_list[os_status.task_list_count]; 
+	task_list[listnum]=task_list[task_list_count]; 
 	//Moving Foward the last list
-	os_status.taks_list_count--;
-	os_status.task_list[os_status.task_list_count]=NULL;
+	taks_list_count--;
+	task_list[task_list_count]=NULL;
 	//Dispose the list
 	return 0;
 }
 
 int add_lib_init(int (*func)()){
-	if(os_status.)
+	if(lib_init_count==MAX_INIT_LIB) return ERR_LIB_INIT_SEQ_OVERFLOW
+	to_init[lib_init_count]=func;
+	lib_init_count++;
 	return 0;
 }
 
